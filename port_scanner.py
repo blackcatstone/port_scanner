659ac8ffd58e9c969c66c17a2a3e765d2dba6fc5
Stone <158994059+jijongjun@users.noreply.github.com>
2024-04-29 11:26:04 +0900

    Revert "Update port_scanner.py"
    This reverts commit a836c644bee515de66a6f97c21a49ab6461fec5d.

--- port_scanner.py
+++ port_scanner.py
@@ -1,27 +1,6 @@
-import socket
+import os
 
-def scan_ports(host, start_port, end_port):
-    # 열려있는 포트 목록
-    open_ports = []
+if __name__ == "__main__":
+    print("main logic")
 
-    # 각 포트에 대해 시도
-    for port in range(start_port, end_port + 1):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(0.1)  # 연결 시도에 대한 타임아웃 설정
-        result = sock.connect_ex((host, port))
-        if result == 0:
-            open_ports.append(port)  # 포트가 열려 있으면 목록에 추가
-        sock.close()
-
-    return open_ports
-
-# 사용 예시
-host = '111.111.111.111'  # 스캔할 대상 IP
-start_port = 1
-end_port = 1024
-open_ports = scan_ports(host, start_port, end_port)
-
-if open_ports:
-    print(f"Open ports on {host}: {open_ports}")
-else:
-    print(f"No open ports on {host}.")
+    os.system("pause")
