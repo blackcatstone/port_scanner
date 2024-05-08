97d0f093a89b009f248b938dcd0f0e83a22795f1
morineee <rawlin0501@gmail.com>
2024-05-09 00:53:53 +0900

    Changed return value to port state instead of boolean

--- ack_scanner.py
+++ ack_scanner.py
@@ -5,11 +5,11 @@ async def ack_scanner(ip, port):
     resp = sr1(IP(dst=ip) / TCP(dport=port, flags="A"), timeout=10.0)
     
     if resp is None:
-        return port, True
+        return port, "Filtered"
     else:
         if resp.haslayer(TCP):
             if resp.getlayer(TCP).flags == 0x4:
-                return port, False
+                return port, "Unfiltered"
         elif resp.haslayer(ICMP):
             if int(resp.getlayer(ICMP).type) == 3 and int(resp.getlayer(ICMP).code) in [1, 2, 3, 9, 10, 13]:
-                return port, True
+                return port, "Filtered"
